04786643b9f932eb30c0077d72fe3fd2c781a6d9
Nine7eeN <pedrorvflorindo@gmail.com>
2026-06-17 16:33:30 -0300

    comentários alterados

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -4,27 +4,28 @@ import dao.UserDAO;
 import entities.User;
 
 public class Program {
-    public static void main(String[] args) {
+    static void main(String[] args) {
 
         UserDAO userDAO = new UserDAO();
 
         //SALVANDO USUARIO
-
-        /*User user = new User(null, "carlosrosa3", "carlos3.rosa@gmail.com");
-        userDAO.save(user);*/
-
+/*
+        User user = new User(null, "carlosrosa3", "carlos3.rosa@gmail.com");
+        userDAO.save(user);
+*/
         //CARREGANDO USUARIOS
-
-        /*for (User u : userDAO.load()) {
+/*
+        for (User u : userDAO.load()) {
             System.out.println(u);
-        }*/
-
+        }
+*/
         //ALTERANDO USUARIO
-
-        /*userDAO.update(1, "joao", "joao@gmail.com");*/
-
+/*
+        userDAO.update(1, "joao", "joao@gmail.com");
+*/
         // DELETANDO USUARIO
-
+/*
             userDAO.delete(1);
+*/
     }
 }
